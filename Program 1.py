fd76c58ba0c775255375ffb195a09f07f5c92852
Alexza Jean Catanoy <alexzacatanoy@gmail.com>
2023-04-18 00:00:52 +0800

    Request

--- Program 1.py
+++ Program 1.py
@@ -10,3 +10,7 @@ print("\033[0;36m*" * 70)
 print("\033[1;32m\nHello! Your programmers name is Alexza Jean.")
 print("\033[1;32m\nShe's from BSCPE 1-4.")
 print("-" * 70)
+
+# Request for users encrypted input and then save it
+input_str = input("\033[0;33m\nKindly type in a encrypted text that I can decrypt?\n")
+output_str = ""
