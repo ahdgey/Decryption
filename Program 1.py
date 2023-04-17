d033fd9fe8886990a444b1b294db3cc82cddd9db
Alexza Jean Catanoy <alexzacatanoy@gmail.com>
2023-04-17 23:52:53 +0800

    Title

--- Program 1.py
+++ Program 1.py
@@ -1,3 +1,8 @@
 # Alexza Jean R. Catanoy
 # BSCPE 1-4
 # Lab Exercise No. 1.2
+
+#Title
+print("\033[0;36m*" * 70)
+print("\033[1;97mDecrypt a Encrypted Text".center(77)) 
+print("\033[0;36m*" * 70)
